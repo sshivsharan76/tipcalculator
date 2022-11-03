10b30c19e5f18e90067df81ce8ca2a392831106b
shubham shivsharan <92040473+sshivsharan76@users.noreply.github.com>
2022-11-03 13:20:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
-function tipCal(){
-    const billAmount=document.getElementById("billamt");
-    const pep=document.getElementById("peopleamt");
-    let tip=0;
-    if(billAmount.value=="")
-    {alert("Please Enter Bill Amount");
-    }
-  //  alert(billAmount.value);
-     else{
-    const service=document.getElementById("serviceQual");
-   // alert(service.value);
-   let pepnum=pep.value;
-   if(pepnum=="" || pepnum==1)
+window.onload = () => {
+    document.querySelector('#calculate').onclick = calculateTip;
+}
+
+function calculateTip()
+{
+    var bill = document.querySelector("#billamt").value;
+    var tipPercentage = document.querySelector("#serviceQual").value;
+    var persons = document.querySelector("#peopleamt").value;
+    if(bill === '' || tipPercentage === 'Select' || persons === '')
     {
-   
-     tip= billAmount.value*service.value;
-    tip=Math.round(tip*100)/100;
-   
-    document.getElementById("tip").innerText="$"+tip;
-   
-    document.getElementById("op").style.display="block";
-     document.getElementById("each").style.display="none";
-  
-   // alert(tip);
-   }
-   else{
-     tip= billAmount.value*service.value/pepnum;
-    tip=Math.round(tip*100)/100;
-    document.getElementById("tip").innerText="$"+tip;
-    document.getElementById("op").style.display="block";
-     document.getElementById("each").style.display="block";
-     }
-}  
+        alert("Please Enter Bill Amount");
+        return;
+    }
+    if(persons === '1')
+    {
+        document.querySelector('#each').style.display = 'none';
+    }
+    else
+    {
+        document.querySelector('#each').style.display = 'block';
+    }
+    var tip=(bill*tipPercentage)/persons;
+    tip=tip.toFixed(2);
+    document.getElementById("total").innerHTML=tip;
 }
